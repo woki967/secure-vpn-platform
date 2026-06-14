794954a1d547c13fb6b16ecaa4b607de5cede19a
woki967 <wojtekwluka@gmail.com>
2026-06-14 12:42:22 +0200

    Configure terraform backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "rg-terraform-state"
     storage_account_name = "tfstate9f2f5afa"
     container_name       = "tfstate"
-    key                  = "validationtest.tfstate"
+    key                  = "secure-vpn-platform.tfstate"
   }
 }
