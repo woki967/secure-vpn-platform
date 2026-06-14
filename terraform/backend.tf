68088091ce8e1c4d51047247ad7f5f72b7b60da4
woki967 <wojtekwluka@gmail.com>
2026-06-14 13:07:32 +0200

    Configure terraform backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-terraform-state"
-    storage_account_name = "tfstate9f2f5afa"
+    storage_account_name = "tfstate5bbd618b"
     container_name       = "tfstate"
     key                  = "secure-vpn-platform.tfstate"
   }
